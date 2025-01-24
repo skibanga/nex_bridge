759dcc3dd012597f0a12607a6ea45275135c533c
Sydney Kibanga <skibanga@aakvatech.com>
2025-01-24 12:12:48 +0300

    feat: Stock Take Doctypes

---
+++ nex_bridge/stock_take/doctype/stock_taker/stock_taker.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Sydney Kibanga and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Stock Taker', {
+	// refresh: function(frm) {
+
+	// }
+});
